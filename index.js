84493d48f126b80ec2f78cfbf22efbd3ffad03b6
Mort Mackof <mmackof@alum.rpi.edu>
2016-08-11 23:49:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var greeting = "";
+var greeting = "Hello, " + "everybody"+"!"
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson "
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation =`Let's talk about ${topic}.`
+function say(greeting, name) {
+  console.log(greeting, `, ${name}!`)
+}
+say("Hi","Willow")
+say(greeting,specialGuest)

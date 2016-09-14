f35b1e2d35fa3840d0a6b470aa63275087e7c169
Zulk Ernan <mand.narsilion@gmail.com>
2016-09-14 06:07:00 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,10 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
+var greeting = "Hello, everybody!";
+var specialGuest = "Neil deGrasse Tyson";
+var topic = "space";
+
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
+var conversation = `Let's talk about ${topic}.`;
 
 it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")

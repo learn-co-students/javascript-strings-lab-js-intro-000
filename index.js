660114d63087d4f9403b35d63d4a5728d71a0a8f
Jenny Huebner <jenny.m.huebner@gmail.com>
2016-09-28 01:20:45 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let\'s talk about space. ";
 
-var conversation = `${topic}`;
+var conversation = `${"topic"}`;

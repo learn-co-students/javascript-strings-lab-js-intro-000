c26a6ed1a42c934df177b245a694b223e3140ce0
Shea Ehrenberger <sheae123@gmail.com>
2016-10-13 21:03:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = Hello,  + specialGuest + !;
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `High + ${3 + 2} + !  Let\'s talk about space.`;

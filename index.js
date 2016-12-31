9da4166bc0a9ea5827bcae6908694e0ad2e54581
Daniel Cruz <cruzdanny87@yahoo.com>
2016-12-31 04:17:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = ("Neil deGrasse Tyson")
+var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest ="Hello, everybody!";+  +"Hello, everybody!";
+var greetSpecialGuest ="Hello, " + specialGuest + "!";
 
 var topic = "space";
 

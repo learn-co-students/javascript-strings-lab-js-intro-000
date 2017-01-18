975327714034080fad66ce2644d99db17feb23fa
Aaron Nickelsberg <anickelsberg18@gmail.com>
2017-01-18 01:57:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var greeting = "";
+var greeting = "Hello, Everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "" + specialGuest + "!";
 

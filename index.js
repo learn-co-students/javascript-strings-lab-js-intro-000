a72bb31f2e1ac66b0b6002a87e8914fda91f2cea
Michal Vacek <vahyno@gmail.com>
2017-02-02 20:45:17 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
-//  var space = 'Let\'s talk about space.'
   
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;

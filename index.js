835696e2c6a4d442f79bee82f30b1b9febaaf297
Abdullah Alam <abdullah.alam.omi@gmail.com>
2017-02-14 08:58:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!"; // defines 'greeting'
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!"; // concatenates strings to greet a special guest in 'greetSpecialGuest'
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}.`;  // interpolates a string in 'conversation' 

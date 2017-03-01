2dd81eb13e916802cf71b3bde0b6ac3d79414cd3
Clebio Vitoriano <cllebyo@hotmail.com>
2017-03-01 17:16:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var greeting = "";
 
-var specialGuest = "Neil deGrasse Tyson"
+var greeting = "Hello, everybody!";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var specialGuest = "Neil deGrasse Tyson";
+
+var greetSpecialGuest = greeting +  specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation =  ${topic};

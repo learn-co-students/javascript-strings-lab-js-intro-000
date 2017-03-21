9752664e6ff74fab917268dab5e1c5497d6fe606
Kavindra Wanniarachchi <kavindra_w@hotmail.com>
2017-03-21 13:46:45 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,16 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
+var greeting = "Hello, everybody!";
+var greetspecialGuest = "Neil deGrasse Tyson!";
+var conversation = "Let's talk about space."
+
+
 
 it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")
 })
 
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
-  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
+  expect(greetSpecialGuest).toEqual(`${specialGuest}!`)
 })
 
 it('interpolates a string in `conversation`', function() {

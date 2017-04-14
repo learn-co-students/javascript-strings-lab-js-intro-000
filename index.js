9840ccebda532ae0ae9758ad2ae7c92e322316dd
Simon Constable <sjeconstable@gmail.com>
2017-04-14 15:07:53 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+greeting = "Hello,"
+var  greeting = "Hello, everybody!";
+var greetSpecialGuest = greeting + specialGuest + "!";
+var greetSpecialGuest = greeting + topic + specialGuest + "!";
+
+var greeting = "Hello!"
+var  greeting = "Hello, everybody!";
+var greetSpecialGuest = "Hello!"+ " " + specialGuest + "!"
+var greetSpecialGuest = "Hello,"+ " " + specialGuest + "!"
+var conversation = "let's talk about" + topic + "."
+var conversation = "Let's talk about" + " " + topic + "."

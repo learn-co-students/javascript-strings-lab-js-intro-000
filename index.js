5417cf32cb6e978a9a2547f7d7e10af8a1c39a4e
racquel giner <racquelginer828@gmail.com>
2017-04-19 02:07:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var greeting = "";
-
-var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest = "" + specialGuest + "!";
-
-var topic = "space";
-
-var conversation = `${topic}`;
+var greeting = "Hello, everybody!";
+var specialGuest = "Angela";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var topic = "dogs";
+var conversation = `Let's talk about ${topic}.`;

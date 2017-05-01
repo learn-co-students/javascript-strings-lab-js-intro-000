06fcfad6d1b3228e8e1905f3eb23c0a350c23fd1
Saint Servulus Billy <saint.billy1@gmail.com>
2017-05-01 21:51:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var greeting = "Hello, everybody!";
-var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!" ;
-var conversation = "Let\'s talk about Let\'s talk about."
-var topic = "Let's talk about";
-var space = "Let's talk about.";
-var conversation = `${topic}` `${space}`;
+var specialGuest = "Neil deGrasse Tyson!"
+
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
+
+var topic = "space";
+
+var conversation = `Let\'s talk about ${topic}.`;

74b32bb8158b9b9db13a04d136aa943ada1e2d12
Stanislaus Kostka <stanislauskostka1@gmail.com>
2017-08-21 11:32:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = ""
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "Hello, !";
 
-var topic = "space";
+var topic = "";
 
-var conversation = `${topic}`;
+var conversation = (`Let's talk about ${topic}.`)

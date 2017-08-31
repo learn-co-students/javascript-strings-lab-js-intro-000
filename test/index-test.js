4fab2fa2138ca64406b1ffad1c3154cef7053e35
harisha Annapareddy <harishaannapareddy@my.unt.edu>
2017-08-31 21:10:25 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,11 +3,13 @@
 it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")
 })
-
+it('defines `greet`', function() {
+  expect(greet).toEqual("Hello")
+})
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
 
-it('interpolates a string in `conversation`', function() {
+it('interpolates `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })

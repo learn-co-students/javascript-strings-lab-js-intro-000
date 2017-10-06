5f973142fb5f5c54114ad21f34f318bb3fd1121d
Kevin-Bourn <bourntwodesign@gmail.com>
2017-10-06 07:07:21 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
 
-it('defines `greeting`', function() {
+it('defines `greeting`', function(Hello, everybody!) {
   expect(greeting).toEqual("Hello, everybody!")
 })
 

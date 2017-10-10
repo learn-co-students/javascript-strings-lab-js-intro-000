aa81c9a2f61075d3a391c1b1c6ab3b4aec33dd90
Hamsa Alchllk <hamsa.b.ch@gmail.com>
2017-10-10 23:12:48 +0000

    Done.

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello," + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Hello, spicalGuest High ${3 + 2}!`;
+var conversation = `Hello, ${spicalGuest} High ${3 + 2}!`;

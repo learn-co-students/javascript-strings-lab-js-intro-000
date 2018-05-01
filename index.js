fb5830b136d1c41e2b97365c0cc07f99ed09e473
Kelsey Swagler <kelsey.swagler@gmail.com>
2018-05-01 15:54:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
-var greetSpecialGuest = 'Hello, ${specialGuest}!';
+var greetSpecialGuest = 'Hello, Neil deGrasse Tyson!';
 
 var topic = "space";
 
-var conversation = 'Let\'s talk about ${topic}.';
+var conversation = "Let's talk about space.";

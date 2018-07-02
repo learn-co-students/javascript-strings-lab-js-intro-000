2b6315f8eb84e80edeb346720d88dddb257c1227
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-07-02 17:31:16 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@
 var greeting = "Hello, everybody!"
 var greetings = "Hello,"
 var specialGuest = " Neil deGrasse Tyson! !"
-var greetSpecialGuest = greetings + specialGuest + "!";
+var greetSpecialGuest = "specialGuest"  + "greetings";
 var topic = "space";
 var conversation = `${topic}`;
 
@@ -10,10 +10,8 @@ it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")
 })
 
-it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
-  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
-})
+it(greetSpecialGuesreetSpecialGuest`', function() {
+  expect(greetSpecialGuest).toEqual"('Hello, ${specialGuest}!`);
 
-it('interpolates a string in `conversation`', function() {
-  expect(conversation).toEqual(`Let's talk about ${topic}.`)
-})
+it('interpolates a string in `conversation`', function()expect(conversation).toEqual ${topic};
+ 

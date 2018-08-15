f5b67daaf031bb13b0604fcf0c78a4a1d128c92b
Jeremy Adams <jadams8411@gmail.com>
2018-08-15 19:25:45 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+var greeting = 'Hello, everybody!'
+var greetSpecialGuest = "Hello" + specialGuest + "!"
+ var greetSpecialGuest = "Hello, " + specialGuest + "!"
+var topic = "Let's talk about space."
+var topic = "space"
+var conversation = "Let's talk about ${topic}."
+var conversation = 'Let\'s talk about ${topic}.'
+var conversation = "Let's talk about space."

c682e3f9bc64ab3ebcbc81b83be454321d15f42e
David Cha <david.cha44@qmail.cuny.edu>
2019-10-10 19:17:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson!";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+
+var greet = "Hello, "
+var greetSpecialGuest = greet.concat(specialGuest).concat("!")
 
 var topic = "space";
-
-var conversation = `${topic}`;
+var sentence = 'Let\'s talk about '
+var conversation = (sentence).concat(topic).concat('.');

c69dbacace9370a5d76e97538d847b5d3c1fe29a
Ian Diesendruck <ian.diesendruck@wework.com>
2019-11-05 14:01:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var d = "Hello, "
+
+var greetSpecialGuest = d + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;

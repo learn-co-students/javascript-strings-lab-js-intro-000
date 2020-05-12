02c3ad42978b8582fc4020157ef63698845d643c
Shelby Ro <shelby_ro@yahoo.com>
2020-05-12 17:05:20 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+var greeting = "Hello, everybody!";
+var specialGuest = "Hello, Neil deGrasse Tyson!"
+var greetSpecialGuest = "Hello, " +specialGuest + "!";
+var topic = "Let's talk about space.";
+var conversation = `Let's talk about ${topic}.`;

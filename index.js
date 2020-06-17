fb588cc56c3e95ef7f1ab689a36fd3896e6f3890
Jonathan Raposo Luna <jonathan.raposo.luna@gmail.com>
2020-06-17 20:17:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `let\'s talk about ${topic}`;

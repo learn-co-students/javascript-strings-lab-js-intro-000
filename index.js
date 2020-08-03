f9f39ac36f267b54102d6789965e5ba1ae2629a8
practical-trainer-5867 <misterjcr@aol.com>
2020-08-03 02:01:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var space = 'Let\'s talk about space.';
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `${space}`;

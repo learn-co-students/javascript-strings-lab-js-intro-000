31edb5de9759d55741f6ccbc3d8247fbe5a81f82
Nicholas Caponi <iamnickygrey@gmail.com>
2020-11-12 15:57:34 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+var greeting = "Hello, everybody!";
+var specialGuest = "Neil"
+`Hello, ${specialGuest}! High`
+"lets/talk/about/space
+"lets/talk"
+`Hello ${specialGuest}! High ${3 + 2}!`

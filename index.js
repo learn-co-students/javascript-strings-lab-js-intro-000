c1d4a3bb3cf1e13854885e7df88286473481cfcc
Jacob Harvey <jacobharv00@gmail.com>
2021-01-05 22:54:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+// var greetSpecial = `Hello, ${person}!`;
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}.`
+
+//-spac
+//     +Let's talk about space.

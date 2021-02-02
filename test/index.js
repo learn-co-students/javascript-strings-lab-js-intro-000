745484cf34bfa7491d50e519414ed04a8bcde7a9
Anmar <anmarma83@gmail.com>
2021-02-02 13:49:11 +0000

    Done.

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello everybody";
+var greeting = "hello";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = greeting + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 

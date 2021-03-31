3ee78ef1ba273b0c25281bf043872c3663fccc05
jovial-validator-023c <rogelio.duarte1988@gmail.com>
2021-03-31 23:19:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson" "Hello, " + specialGuest + "!"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
